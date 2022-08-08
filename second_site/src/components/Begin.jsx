9d8881f6bb3f35cafba9f0887ebd08a7dcbaba2b
soroush heidary <93920906+soroushheidary99@users.noreply.github.com>
2022-08-08 14:55:45 +0430

    Add files via upload

--- second_site/src/components/Begin.jsx
+++ second_site/src/components/Begin.jsx
@@ -22,11 +22,11 @@ function Begin() {
         
     return (
         <div className='beginPage' id='beginPage'>
-            <p className='begin_p'>This is still development, and I am still new to the web dev world, for now it is suggested that you use a widescreen display</p>
+            <p className='begin_p'>This site is still under development, and I am still new to the web dev world, for now it is suggested that you use a widescreen display</p>
             <p className='begin_p begin_p2'>Also be sure to checkout the game of life section, personally i think the Conway's Game of Life is beyond underrated</p>
             <div className='begin_buttons'>
                 <Button id='BeginButton' onClick={() => Begin()}>I Undrestand :D</Button>
-                <Button id='BeginButton'>I Will Sue You!</Button>
+                <Button id='BeginButton'>No :/</Button>
             </div>
         </div>
     )
